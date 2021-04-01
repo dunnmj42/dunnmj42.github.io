b2b1bd8f02290b968c745f0dbc0483d5f6387417
Michael Dunn <dunnmj42@gmail.com>
2021-04-01 11:28:42 -0500

    Darkness

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App.jsx';
 
+// MUI
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+
+// Theme/Palette/Font declaration
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
